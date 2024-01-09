d9bb29d7594f8af6511b4e0acc34445c05b313c8
Shafi_2065 <shafimiah234@gmail.com>
2024-01-09 23:52:03 +0000

    v-4
    added TopNav only for logged in users (needs logout and profile view)
    Changed some styling

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,41 @@ import TopNav from "../components/topNav";
 import "../public/style.css";
 import "./globals.css";
 import { useEffect } from "react";
+<<<<<<< Updated upstream
 
 export default function Index() {
+=======
+import * as React from "react";
+import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import './firebaseAuth';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Index() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    });
+
+    // Cleanup function to unsubscribe from onAuthStateChanged
+    return () => unsubscribe();
+  }, []);
+  // Define the timeline for the banner animations
+>>>>>>> Stashed changes
   useEffect(() => {
     var t1 = gsap.timeline();
 
@@ -15,10 +48,60 @@ export default function Index() {
     t1.add(tween2);
   }, []);
 
+<<<<<<< Updated upstream
   return (
     <>
       <div className="homeContainer">
       <TopNav />
+=======
+  // Define the animations for the explore containers
+  const exploreAnimations = () => {
+    var t2 = gsap.timeline();
+    // Trigger animations when the exploreContainer comes into view
+    gsap.fromTo("#exploreContainer", {
+      opacity: 0,
+      y: -200,
+      duration: 1,
+    },
+      {
+        opacity: 1,
+        y: 0,
+      scrollTrigger: {
+        trigger: "#exploreContainer",
+        ease: "power1.inOut",
+        pin: false,
+        start: "center center"
+      },
+    });
+
+    // Trigger animations when the exploreContainer2 comes into view
+    gsap.fromTo("#exploreContainer2", {
+      opacity: 0,
+      x: -200,
+      duration: 1,
+    },
+    {
+      opacity: 1,
+      x: 0,
+      delay: 1,
+      scrollTrigger: {
+        trigger: "#exploreContainer2",
+        ease: "power1.inOut",
+        pin: false,
+        start: "bottom bottom"
+      },
+    });
+  };
+
+  useEffect(() => {
+    exploreAnimations();
+  }, []);
+
+  return (
+    <>
+      <div className="homeContainer">
+      {isAuthenticated && <TopNav />}
+>>>>>>> Stashed changes
         <div className="bannerContent">
           <h1 className="bannerText">Find Your Place.</h1>
           <button className="bannerButton">Explore</button>
--- components/loginform.jsx
+++ components/loginform.jsx
@@ -3,12 +3,63 @@ import React, { useState } from "react";
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
+<<<<<<< Updated upstream
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("A username was submitted " + username);
     console.log("A password was submitted " + password);
 
+=======
+  const handleLogin = (e) => {
+    e.preventDefault();
+    
+    setErrorMessage("");
+
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        Swal.fire({
+          title: "Successfully Logged in",
+          text: "redirecting to another page",
+          icon: "success"
+        });
+      })
+      .catch((error) => {
+        console.error(error.code, error.message);
+        setErrorMessage("Invalid Login");
+      });
+  };
+
+  const googleLogin = () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+    getRedirectResult(auth)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+>>>>>>> Stashed changes
   };
   return (
     <div>
@@ -29,6 +80,7 @@ function Login() {
             value={password}
             onChange={(e) => handlePassword(e.target.value)}
           />
+          <p>{errorMessage}</p>
         </label>
         <button type="submit">Login</button>
         <a href="#">Register an account today!</a>
